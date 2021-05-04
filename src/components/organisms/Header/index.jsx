b5a2ff08c579e900707f1a5fcaa44a28f81eba1b
bnn848 <77334083+bnn848@users.noreply.github.com>
2021-05-04 17:02:03 +0900

    youtube viewer

---
+++ src/components/organisms/Header/index.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import NavigationLinks from '~/components/organisms/NavigationLinks';
+import Typography from '~/components/atoms/Typography';
+
+const Root = styled.div`
+  width: 100%;
+`;
+
+const StyledNavigationLinks = styled(NavigationLinks)`
+  margin-left: auto;
+`;
+
+const Header = ({ className }) => (
+  <Root className={className}>
+    <Typography size="title" color="red" align="left">
+      YouTube Viewer
+    </Typography>
+    <StyledNavigationLinks />
+  </Root>
+);
+
+Header.propTypes = {
+  className: PropTypes.string,
+};
+
+Header.defaultProps = {
+  className: '',
+};
+
+export default Header;
